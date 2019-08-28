f8c876b07c1d50aefc77d01601e5e349cbe65158
Tyler Long <tyler4long@gmail.com>
2019-08-28 11:52:28 -0700

    First working version

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ## Setup
 
 ```
+bundle install
 cp .env.sample .env
 ```
 
@@ -19,4 +20,26 @@ ruby index.rb
 
 ## Usage
 
-HTTP Post to http://localhost:3000 with the following parameters
+HTTP Post to http://localhost:4567 with the following parameters:
+
+- receiver
+- text
+- file_url
+- file_type
+
+For example:
+
+```
+curl -X POST http://localhost:4567 \
+-F "receiver=6508888888" \
+-F "text=hello world" \
+-F "file_url=https://www.baidu.com/img/bd_logo1.png" \
+-F "file_type=image/png"
+```
+
+
+## Deploy
+
+You can deploy this app to any server with a public adress, so that you can access it from anywhere.
+
+You can also add some [password protection](http://recipes.sinatrarb.com/p/middleware/rack_auth_basic_and_digest) so that only you can use this service.
--- index.rb
+++ index.rb
@@ -1,8 +1,37 @@
 require 'dotenv/load'
 require 'sinatra'
+require 'down'
+require 'ringcentral'
+
+RINGCENTRAL_SERVER_URL = ENV['RINGCENTRAL_SERVER_URL']
+RINGCENTRAL_CLIENT_ID = ENV['RINGCENTRAL_CLIENT_ID']
+RINGCENTRAL_CLIENT_SECRET = ENV['RINGCENTRAL_CLIENT_SECRET']
+RINGCENTRAL_USERNAME = ENV['RINGCENTRAL_USERNAME']
+RINGCENTRAL_EXTENSION = ENV['RINGCENTRAL_EXTENSION']
+RINGCENTRAL_PASSWORD = ENV['RINGCENTRAL_PASSWORD']
 
 post '/' do
     status 200
+    receiver = params['receiver']
+    text = params['text']
+    file_url = params['file_url']
+    file_type = params['file_type']
 
+    tempfile = Down.download(file_url)
+    file_type ||= tempfile.content_type
+
+    rc = RingCentral.new(RINGCENTRAL_CLIENT_ID, RINGCENTRAL_CLIENT_SECRET, RINGCENTRAL_SERVER_URL)
+    rc.authorize(username: RINGCENTRAL_USERNAME, extension: RINGCENTRAL_EXTENSION, password: RINGCENTRAL_PASSWORD)
+    r = rc.post('/restapi/v1.0/account/~/extension/~/sms',
+        payload: {
+            to: [{ phoneNumber: receiver }],
+            from: { phoneNumber: RINGCENTRAL_USERNAME },
+            text: text
+        },
+        files: [
+            [tempfile.path, file_type]
+        ]
+    )
+    rc.revoke()
     body 'OK'
 end
